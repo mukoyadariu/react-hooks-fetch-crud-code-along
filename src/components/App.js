f3b83bb46c1c77abc1a46b6306782b61f3f0c9b9
mukoyadarius <mukoya.darius@student.moringaschool.com>
2023-07-25 12:26:28 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchShoppingList();
+  }, []);
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  const fetchShoppingList = () => {
+    fetch("http://localhost:4000") // Assuming the server is running on http://localhost:4000
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => setItems(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  };
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList />
+      <ShoppingList items={items} />
     </div>
   );
 }
